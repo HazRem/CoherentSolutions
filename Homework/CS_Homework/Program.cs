275b8c30a7d878f4177734ec8e1e0ae69729a245
Remigiusz Haziak <haziakremigiusz@gmail.com>
2024-10-16 23:06:41 +0200

    Task1 completion

---
+++ Homework/CS_Homework/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Please input the first number...");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Please input the second number...");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+    SwapVariableValues(firstNumber, secondNumber);
+Console.WriteLine("Instances of numbers with 2 A's in duodecimal system:");
+for (int i = firstNumber; i <= secondNumber; i++)
+{
+    string duodecimal = ConvertToDuoDecimal(i);
+    if (CountACharOccurences(duodecimal) == 2)
+        Console.WriteLine(i);
+}
+Console.WriteLine("Press any key to leave...");
+Console.ReadKey();
+
+
+static string ConvertToDuoDecimal(int number)
+{
+    if (number == 0)
+        return "0";
+
+    int remainder = number % 12;
+    string result;
+
+    if (remainder == 10)
+        result = "A";
+    else if (remainder == 11)
+        result = "B";
+    else
+        result = remainder.ToString();
+
+    if (number / 12 > 0)
+        return ConvertToDuoDecimal(number / 12) + result;
+    else
+        return result;
+}
+
+static int CountACharOccurences(string input)
+{
+    int count = 0;
+    foreach (char c in input)
+        if (c == 'A')
+            count++;
+    return count;
+}
+
+static void SwapVariableValues(int a, int b)
+{
+    int tempInt = b;
+    b = a;
+    a = tempInt;
+}
---
+++ Homework/Task1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter the first 9 digits of the ISBN:");
+string isbn9 = Console.ReadLine();
+
+int sum = 0;
+int checkDigit;
+
+for (int i = 0; i < 9; i++)
+{
+    sum += (10 - i) * (isbn9[i] - '0');
+}
+
+checkDigit = 11 - (sum % 11);
+
+string isbn10 = isbn9 + (checkDigit == 10 ? "X" : checkDigit.ToString());
+Console.WriteLine("The complete 10-digit ISBN is: " + isbn10);
